33dce9827127cd1a0a18162a09c1adbf6b969ef2
amandats4i <amandatsai16@gmail.com>
2023-12-05 11:07:53 -0300

    Inserindo JOINS

--- aula_02/quitanda.sql
+++ aula_02/quitanda.sql
@@ -1,56 +1,155 @@
+-- Apagar Banco de Dados db_quitanda
+DROP DATABASE db_quitanda;
 
+-- Recriar o Banco de dados db_quitanda
+CREATE DATABASE db_quitanda;
+
+-- Selecionar o Banco de Dados db_quitanda
+USE db_quitanda;
+
+-- Criar a tabela tb_categorias
+CREATE TABLE tb_categorias(
+id bigint AUTO_INCREMENT,
+descricao varchar(255) NOT NULL,
+PRIMARY KEY (id)
+);
+
+-- Insere dados na tabela tb_categorias
+INSERT INTO tb_categorias (descricao)
+VALUES ("Frutas");
+
+INSERT INTO tb_categorias (descricao)
+VALUES ("Verduras");
+
+INSERT INTO tb_categorias (descricao)
+VALUES ("Legumes");
+
+INSERT INTO tb_categorias (descricao)
+VALUES ("Temperos");
+
+
+INSERT INTO tb_categorias (descricao)
+VALUES ("Ovos");
+
+INSERT INTO tb_categorias (descricao)
+VALUES ("outros");
+
+-- Lista todos os dados da tabela tb_categorias
+SELECT * FROM tb_categorias;
+
+-- Criar a Tabela tb_produtos
 CREATE TABLE tb_produtos(
 id bigint AUTO_INCREMENT,
 nome varchar(255) NOT NULL,
 quantidade int, 
-data_validade date,
-preco decimal NOT NULL, 
-PRIMARY KEY (id)
+dtvalidade date NULL,
+preco decimal(6, 2),
+categoria_id bigint, 
+PRIMARY KEY (id),
+FOREIGN KEY (categoria_id) REFERENCES tb_categorias(id)
 );
 
-INSERT INTO tb_produtos(nome, quantidade, data_validade, preco)
-VALUES ("tomate",100, "2022-04-10", 8.00);
+-- Insere dados na tabela tb_produtos
+INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categoria_id)
+VALUES ("Maçã", 1000, "2022-03-07", 1.99, 1);
 
-INSERT INTO tb_produtos(nome, quantidade, data_validade, preco)
-VALUES ("maçã",20, "2022-04-12", 5.00);
+INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categoria_id)
+VALUES ("Banana", 1300, "2022-03-08", 5.00, 1);
 
-INSERT INTO tb_produtos(nome, quantidade, data_validade, preco)
-VALUES ("laranja",50, "2022-04-10", 10.00);
+INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categoria_id)
+VALUES ("Batata doce", 2000, "2022-03-09", 10.00, 3);
 
-INSERT INTO tb_produtos(nome, quantidade, data_validade, preco)
-VALUES ("banana",200, "2022-04-13", 12.00);
+INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categoria_id)
+VALUES ("Alface", 300, "2022-03-10", 7.00, 2);
 
-INSERT INTO tb_produtos(nome, quantidade, data_validade, preco)
-VALUES ("uva",1200, "2022-04-14", 30.00);
+INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categoria_id)
+VALUES ("Cebola", 1020, "2022-03-08", 5.00, 3);
 
-INSERT INTO tb_produtos(nome, quantidade, data_validade, preco)
-VALUES ("pêra",500, "2022-04-15", 2.90);
+INSERT INTO tb_produtos(nome, quantidade, dtvalidade, preco, categoria_id)
+VALUES("Ovo Branco", 1000, "2022-03-07", 15.00, 5);
 
-select * from tb_produtos;
+INSERT INTO tb_produtos(nome, quantidade, dtvalidade, preco, categoria_id)
+VALUES("Agrião", 1500, "2022-03-06", 3.00, 2);
 
-SELECT nome, preco FROM tb_produtos;
+INSERT INTO tb_produtos(nome, quantidade, dtvalidade, preco, categoria_id)
+VALUES("Cenoura", 1800, "2022-03-09", 3.50, 3);
 
--- Visualizar somente o nome e o preço formatado para Reais
-SELECT nome_produto, CONCAT('R$ ', FORMAT(preco, 2, 'pt_BR')) AS valor FROM tb_produtos;
+INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categoria_id)
+VALUES ("Pimenta", 1100, "2022-03-15", 10.00, 4);
 
-ALTER TABLE tb_produtos CHANGE nome nome_produto varchar(255);
+INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categoria_id)
+VALUES ("Alecrim", 130, "2022-03-10", 5.00, 4);
 
-SELECT nome_produto, DATE_FORMAT(data_validade, '%d/%m/%Y') AS Data_Validade FROM tb_produtos;
+INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categoria_id)
+VALUES ("Manga", 200, "2022-03-07", 5.49, 1);
 
-SELECT * FROM tb_produtos WHERE id = 1;
+INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categoria_id)
+VALUES ("Laranja", 3000, "2022-03-13", 10.00, 1);
 
-SELECT * FROM tb_produtos WHERE preco > 5.00;
+INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categoria_id)
+VALUES ("Couve", 100, "2022-03-12", 1.50, 2);
 
-SELECT * FROM tb_produtos WHERE nome_produto = "maçã";
+INSERT INTO tb_produtos(nome, quantidade, dtvalidade, preco, categoria_id)
+VALUES("Tomate", 1105, "2022-03-15", 3.00, 3);
 
-ALTER TABLE tb_produtos ADD descricao varchar(255);
+INSERT INTO tb_produtos(nome, quantidade, dtvalidade, preco, categoria_id)
+VALUES("Rabanete", 1200, "2022-03-15", 13.00, 3);
 
-ALTER TABLE tb_produtos MODIFY preco decimal(6, 2);
+INSERT INTO tb_produtos(nome, quantidade, preco)
+VALUES("Sacola Cinza", 1118, 0.50);
 
-UPDATE tb_produtos SET preco = 2.90 WHERE id = 1;
+INSERT INTO tb_produtos(nome, quantidade, preco)
+VALUES("Sacola Verde", 1118, 0.50);
 
-DELETE FROM tb_produtos WHERE id = 2;
+-- Visualiza todos os dados da tabela tb_produtos
+SELECT * FROM tb_produtos;
 
+SELECT * FROM tb_produtos ORDER BY nome, preco;
 
+SELECT * FROM tb_produtos ORDER BY nome DESC;
 
+SELECT * FROM tb_produtos WHERE preco IN (5, 10, 15);
 
+SELECT * FROM tb_produtos WHERE preco BETWEEN 5 and 15;
+
+SELECT * FROM tb_produtos WHERE nome LIKE "%ra%"; -- produtos que possuem da SÍLABA "ra"
+
+SELECT * FROM tb_produtos WHERE nome LIKE "%ra"; -- produtos que TERMINAM COM "ra"
+
+SELECT * FROM tb_produtos WHERE nome LIKE "ra%"; -- produtos que COMEÇAM com "ra"
+
+SELECT count(*) FROM tb_produtos; -- conta linha da tabela
+
+SELECT sum(preco) FROM tb_produtos; -- soma os valores da coluna preco
+
+SELECT avg(preco) FROM tb_produtos; -- mostra a média da coluna preco
+
+SELECT max(preco) FROM tb_produtos; -- mostra o valor mais alto
+
+SELECT min(preco) FROM tb_produtos; -- mostra o menor valor da tabela
+
+-- duas tabelas com seus elementos especificos estão sendo juntadas
+SELECT nome, preco, quantidade, tb_categorias.descricao 
+FROM tb_produtos INNER JOIN tb_categorias  
+ON tb_produtos.categoria_id = tb_categorias.id;
+
+SELECT nome, preco, quantidade, tb_categorias.descricao 
+FROM tb_produtos LEFT JOIN tb_categorias  
+ON tb_produtos.categoria_id = tb_categorias.id;
+
+SELECT nome, preco, quantidade, tb_categorias.descricao 
+FROM tb_produtos RIGHT JOIN tb_categorias  
+ON tb_produtos.categoria_id = tb_categorias.id;
+
+UPDATE tb_produtos SET categoria_id = 6 WHERE id = 16;
+
+-- Aqui, vamos pegar cada elemento da coluna "descrição", somar e entregar a média de cada grupo de "descrição"
+SELECT tb_categorias.descricao, avg(preco) AS Preço_Médio  
+FROM tb_produtos INNER JOIN tb_categorias  
+ON tb_produtos.categoria_id = tb_categorias.id
+GROUP BY tb_categorias.descricao; -- agrupará o resultado por "descrição"
+
+SELECT *  
+FROM tb_produtos INNER JOIN tb_categorias  
+ON tb_produtos.categoria_id = tb_categorias.id
+WHERE tb_categorias.id = 2; 
